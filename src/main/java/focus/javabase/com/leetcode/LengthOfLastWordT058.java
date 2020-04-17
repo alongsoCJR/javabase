fc8e7dd61427fd2e911cd3ef628817f4be7df162
alongsoCJR <alongsocjr@163.com>
2020-04-17 13:43:41 +0800

    最后一个单词长度

---
+++ src/main/java/focus/javabase/com/leetcode/LengthOfLastWordT058.java
@@ -0,0 +1,32 @@
+package focus.javabase.com.leetcode;
+
+//给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+//
+// 如果不存在最后一个单词，请返回 0 。
+//
+// 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+//
+//
+//
+// 示例:
+//
+// 输入: "Hello World"
+//输出: 5
+
+public class LengthOfLastWordT058 {
+
+    // "a "  1
+    public static int lengthOfLastWord(String s) {
+        assert s != null;
+        String string = s.trim();
+        int i = 0, j;
+        for (j = string.length() - 1; j >= 0; j--) {
+            char c = string.charAt(j);
+            if (c == ' ') {
+                break;
+            }
+            i++;
+        }
+        return i;
+    }
+}
---
+++ src/test/java/focus/javabase/com/leetcode/LengthOfLastWordT058Test.java
@@ -0,0 +1,29 @@
+package focus.javabase.com.leetcode;
+
+import focus.javabase.com.baseTest.AbstractTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LengthOfLastWordT058Test extends AbstractTest {
+
+//    @Before
+    public void setUp() throws Exception {
+        inputStr = "Hello World";
+        exceptedNum = 5;
+    }
+
+    @Before
+    public void setUp1() throws Exception {
+        inputStr = "a ";
+        exceptedNum = 1;
+    }
+
+    @Test
+    public void lengthOfLastWord() {
+        outputNum = LengthOfLastWordT058.lengthOfLastWord(inputStr);
+        Assert.assertEquals(exceptedNum, outputNum);
+    }
+}
