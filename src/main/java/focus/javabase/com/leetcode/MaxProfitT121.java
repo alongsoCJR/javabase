bb2676a7f2c67c51667613fa3e32c753516ebd1f
alongsoCJR <alongsocjr@163.com>
2020-05-11 13:20:56 +0800

    买卖股票最好的时机 优化

--- src/main/java/focus/javabase/com/leetcode/MaxProfitT121.java
+++ src/main/java/focus/javabase/com/leetcode/MaxProfitT121.java
@@ -8,6 +8,18 @@ public class MaxProfitT121 {
         int maxProfit = 0;
         int len = prices.length;
         for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len && prices[j] > prices[i]; j++) {
+                maxProfit = Integer.max(prices[j] - prices[i], maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+
+
+    public static int maxProfit1(int[] prices) {
+        int maxProfit = 0;
+        int len = prices.length;
+        for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
                 maxProfit = Integer.max(prices[j] - prices[i], maxProfit);
             }
