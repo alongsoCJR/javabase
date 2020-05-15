95b9908dd12c18ecebbbae4b76d590e90e74b54a
alongsoCJR <alongsocjr@163.com>
2020-05-15 09:55:37 +0800

    验证回文串

---
+++ src/main/java/focus/javabase/com/leetcode/IsPalindromeT125.java
@@ -0,0 +1,40 @@
+package focus.javabase.com.leetcode;
+
+/**
+ * @author chenjianrong-lhq 2019年08月03日 17:04:48
+ * @Description: 是否是回文字符串
+ * @ClassName: IsPalindromeT009
+ */
+public class IsPalindromeT125 {
+
+
+    //i < arr.length - 1 && (arr[i] == ' ' || arr[i] == ';' || arr[i] == ',' || arr[i] == ':'
+    public static boolean isPalindrome(String s) {
+        assert s != null;
+        char[] arr = s.toUpperCase().toCharArray();
+        int i = 0, j = arr.length - 1;
+        while (i <= j) {
+            char iChar = arr[i];
+            char jChar = arr[j];
+            while ((iChar < '0' || iChar > '9' && iChar < 'A' || iChar > 'Z')) {
+                if (i >= arr.length - 1) {
+                    return true;
+                }
+                iChar = arr[++i];
+            }
+            while ((jChar < '0' || jChar > '9' && jChar < 'A' || jChar > 'Z')) {
+                if (j <= 0) {
+                    return true;
+                }
+                jChar = arr[--j];
+            }
+            if (iChar != jChar) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/focus/javabase/com/leetcode/IsPalindromeT125Test.java
@@ -0,0 +1,76 @@
+package focus.javabase.com.leetcode;
+
+import focus.javabase.com.baseTest.AbstractTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//Given a string, determine if it is a palindrome, considering only alphanumeric
+// characters and ignoring cases.
+//
+// Note: For the purpose of this problem, we define empty string as valid palind
+//rome.
+//
+// Example 1:
+//
+//
+//Input: "A man, a plan, a canal: Panama"
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input: "race a car"
+//Output: false
+//
+// Related Topics 双指针 字符串
+
+
+public class IsPalindromeT125Test extends AbstractTest {
+
+    //    @Before
+    public void setUp() throws Exception {
+        inputStr = "A man, a plan, a canal: Panama";
+        exceptedBoolean = true;
+    }
+
+
+    //    @Before
+    public void setUp1() throws Exception {
+        inputStr = "race a car";
+        exceptedBoolean = false;
+    }
+
+    //    @Before
+    public void setUp2() throws Exception {
+        inputStr = ".,";
+        exceptedBoolean = true;
+    }
+
+    @Before
+    public void setUp3() throws Exception {
+        inputStr = "0P";
+        exceptedBoolean = false;
+    }
+//info
+//			Wrong Answer:
+//			input:".,"
+//			Output:false
+//			Expected:true
+//			stdout:
+
+
+    //info
+//			Wrong Answer:
+//			input:"0P"
+//			Output:true
+//			Expected:false
+//			stdout:
+    @Test
+    public void isPalindrome() {
+        Assert.assertEquals(exceptedBoolean, IsPalindromeT125.isPalindrome(inputStr));
+    }
+}
