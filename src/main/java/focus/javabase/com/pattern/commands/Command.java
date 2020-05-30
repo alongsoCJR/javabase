f8d6e5b25d538e2adb566651dfc30c0060368059
alongsoCJR <alongsocjr@163.com>
2020-05-30 15:58:43 +0800

    命令模式 代码实现 类图UML图片提交

---
+++ src/main/java/focus/javabase/com/pattern/commands/Command.java
@@ -0,0 +1,6 @@
+package focus.javabase.com.pattern.commands;
+
+public interface Command {
+
+    void execute();
+}
---
+++ src/main/java/focus/javabase/com/pattern/commands/GarageDoor.java
@@ -0,0 +1,8 @@
+package focus.javabase.com.pattern.commands;
+
+public class GarageDoor {
+
+    public void up() {
+        System.out.println("GarageDoor is open!");
+    }
+}
