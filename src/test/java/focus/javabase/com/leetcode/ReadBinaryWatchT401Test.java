dfe2548d9ab1e4bc5f5665c68a7907399ed3e6d7
alongsoCJR <alongsocjr@163.com>
2020-06-13 16:39:42 +0800

    二进制手表 参考

---
+++ src/test/java/focus/javabase/com/leetcode/ReadBinaryWatchT401Test.java
@@ -0,0 +1,54 @@
+package focus.javabase.com.leetcode;
+
+import focus.javabase.com.baseTest.AbstractTest;
+import focus.javabase.com.baseTest.VerifyUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+//A binary watch has 4 LEDs on the top which represent the hours (0-11), and the
+// 6 LEDs on the bottom represent the minutes (0-59).
+// Each LED represents a zero or one, with the least significant bit on the righ
+//t.
+//
+// For example, the above binary watch reads "3:25".
+//
+// Given a non-negative integer n which represents the number of LEDs that are c
+//urrently on, return all possible times the watch could represent.
+//
+// Example:
+// Input: n = 1 Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04",
+// "0:08", "0:16", "0:32"]
+//
+//
+// Note:
+//
+// The order of output does not matter.
+// The hour must not contain a leading zero, for example "01:00" is not valid, i
+//t should be "1:00".
+// The minute must be consist of two digits and may contain a leading zero, for
+//example "10:2" is not valid, it should be "10:02".
+//
+// Related Topics 位运算 回溯算法
+
+
+public class ReadBinaryWatchT401Test extends AbstractTest {
+
+    @Before
+    public void setUp() throws Exception {
+        inputNum = 1;
+        exceptedStrs = new String[]{"1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04",
+                "0:08", "0:16", "0:32"};
+        exceptedArrayList = Arrays.asList(exceptedStrs);
+    }
+
+    @Test
+    public void readBinaryWatch() {
+        outputArrayList = ReadBinaryWatchT401.readBinaryWatch(inputNum);
+        VerifyUtils.verifyList(exceptedArrayList, outputArrayList);
+    }
+}
