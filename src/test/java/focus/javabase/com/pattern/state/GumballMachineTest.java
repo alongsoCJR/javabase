4202aa57e75634014496875615acf0338018bc5f
alongsoCJR <alongsocjr@163.com>
2020-06-19 18:02:53 +0800

    状态模式 糖果机 初版实现

---
+++ src/test/java/focus/javabase/com/pattern/state/GumballMachineTest.java
@@ -0,0 +1,43 @@
+package focus.javabase.com.pattern.state;
+
+import focus.javabase.com.baseTest.AbstractTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GumballMachineTest extends AbstractTest {
+
+    @Test
+    public void insertQuarter() {
+
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuater();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuater();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+}
