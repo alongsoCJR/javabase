26a3d9b1bd638c0f34d9dedfac38cfcfc46d0a30
alongsoCJR <alongsocjr@163.com>
2020-06-21 18:41:13 +0800

    回文链表 参考

--- src/main/java/focus/javabase/com/leetcode/IsPalindromeNodeT234.java
+++ src/main/java/focus/javabase/com/leetcode/IsPalindromeNodeT234.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class IsPalindromeNodeT234 {
 
-    // 哈希表
+    // 哈希表(也能用arrayList实现)  时间复杂度:O(n) 空间复杂度：O(n)
     public static boolean isPalindrome(ListNode head) {
         Map<Integer, Integer> nodeMap = new HashMap<>();
         int index = 0;
@@ -26,4 +26,52 @@ public class IsPalindromeNodeT234 {
         }
         return true;
     }
+
+    // 参考
+    public static boolean isPalindrome1(ListNode head) {
+
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private static ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
 }
