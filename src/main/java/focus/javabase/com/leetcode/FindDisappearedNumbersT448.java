dfbb2b5509cb62ef3e11baa596b329fce7f81087
alongsoCJR <alongsocjr@163.com>
2020-07-02 15:01:15 +0800

    找到数组中缺少的元素 参考

--- src/main/java/focus/javabase/com/leetcode/FindDisappearedNumbersT448.java
+++ src/main/java/focus/javabase/com/leetcode/FindDisappearedNumbersT448.java
@@ -70,7 +70,70 @@ public class FindDisappearedNumbersT448 {
         return retList;
     }
 
+    // 参考 优化，更容易理解
+    public static List<Integer> findDisappearedNumbers3(int[] nums) {
+        List<Integer> retList = new ArrayList<>();
+        // 置负数
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -1 * (nums[index]);
+            }
+        }
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                retList.add(i + 1);
+            }
+        }
+
+        return retList;
+    }
+
+
+    // 参考  抽屉原理 + 基于“异或运算”交换两个变量的值
+    public static List<Integer> findDisappearedNumbers4(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+    private static void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+    }
+
+
+    public List<Integer> findDisappearedNumbers5(int[] nums) {
+        int n = nums.length;
+        boolean[] map = new boolean[n + 1];
+        for (int x : nums) {
+            map[x] = true;
+        }
+
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            if (!map[i]) {
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
     // for (int i = 1; i <= nums.length; i++) {
     //            retList.add(i);
     //        }
