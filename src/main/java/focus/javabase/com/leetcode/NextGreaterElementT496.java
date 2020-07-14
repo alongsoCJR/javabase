e64cfadb5a86cd6ee2c1f592498f63ff8532b67b
alongsoCJR <alongsocjr@163.com>
2020-07-14 10:07:03 +0800

    下一个更大的元素 I 参考

--- src/main/java/focus/javabase/com/leetcode/NextGreaterElementT496.java
+++ src/main/java/focus/javabase/com/leetcode/NextGreaterElementT496.java
@@ -1,17 +1,13 @@
 package focus.javabase.com.leetcode;
 
-import com.sun.tools.classfile.Annotation;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class NextGreaterElementT496 {
 
+    // 时间复杂度为O(N^2)
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-//        Stack<Integer> stack = new Stack<>();
-//        for (int i = 0; i < nums2.length; i++) {
-//            stack.push(nums1[i]);
-//        }
-
         for (int i = 0; i < nums1.length; i++) {
             int num = nums1[i];
             int val = -1;
@@ -29,6 +25,22 @@ public class NextGreaterElementT496 {
             }
             nums1[i] = val;
 
+        }
+        return nums1;
+    }
+
+    // 参考 时间复杂度O(N)
+    public static int[] nextGreaterElement1(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                numsMap.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = numsMap.getOrDefault(nums1[i], -1);
         }
         return nums1;
     }
