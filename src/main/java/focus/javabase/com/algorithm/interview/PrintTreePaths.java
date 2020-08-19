18f9e88d9bad4574775fef9942d73d7fddd36e6b
alongsoCJR <alongsocjr@163.com>
2020-08-19 09:43:00 +0800

    KS 面试算法题 打印二叉树的所有路径

---
+++ src/main/java/focus/javabase/com/algorithm/interview/PrintTreePaths.java
@@ -0,0 +1,91 @@
+package focus.javabase.com.algorithm.interview;
+
+import focus.javabase.com.leetcode.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+/**
+ * @Author chenjianrong-lhq
+ * @Description ks面试题
+ * @Date 2020-08-19 09:36
+ **/
+public class PrintTreePaths {
+
+
+    public static List print(TreeNode root) {
+        if (root == null) {
+            return new ArrayList();
+        }
+        List<List<Integer>> result = new ArrayList();
+        Stack<Integer> stack = new Stack();
+        // 遍历左子树 [2,4] [2,5]
+        List<Stack<Integer>> leftResult = print(root.left);
+        // 遍历右子树 [3]
+        List<Stack<Integer>> rightResult = print(root.right);
+
+        // 放入结果集
+        if (leftResult.size() > 0) {
+            // 将当前元素加至下标为0的位置
+            for (Stack<Integer> s : leftResult) {
+                s.push(root.val);
+                result.add(s);
+            }
+        }
+
+        if (rightResult.size() > 0) {
+            // 将当前元素加至下标为0的位置
+            for (Stack<Integer> s : rightResult) {
+                s.push(root.val);
+                result.add(s);
+            }
+        }
+
+        if (leftResult.size() == 0 && rightResult.size() == 0) {
+            stack.push(root.val);
+            result.add(stack);
+        }
+        return result;
+    }
+
+    public static List print1(TreeNode root) {
+        if (root == null) {
+            return new ArrayList();
+        }
+        List<List<Integer>> result = new ArrayList();
+        List<Integer> numsList = new ArrayList();
+        // 遍历左子树 [2,4] [2,5] [2]
+        List<List<Integer>> leftResult = print1(root.left);
+        // 遍历右子树 [3]
+        List<List<Integer>> rightResult = print1(root.right);
+
+        // 放入结果集
+        if (leftResult.size() > 0) {
+            // 将当前元素加至下标为0的位置
+            for (List list : leftResult) {
+                List subList = new ArrayList();
+                subList.add(root.val);
+                subList.addAll(list);
+                result.add(subList);
+            }
+        }
+
+        if (rightResult.size() > 0) {
+            // 将当前元素加至下标为0的位置
+            for (List list : rightResult) {
+                List subList = new ArrayList();
+                subList.add(root.val);
+                subList.addAll(list);
+                result.add(subList);
+            }
+        }
+        // 叶子节点
+        if (leftResult.size() == 0 && rightResult.size() == 0) {
+            numsList.add(root.val);
+            result.add(numsList);
+        }
+        return result;
+    }
+}
