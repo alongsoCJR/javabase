3a98117abb11c552f504a4a99700e9a24dd5e947
alongsoCJR <alongsocjr@163.com>
2020-09-15 09:27:10 +0800

    交替位二进制数 参考

---
+++ src/main/java/focus/javabase/com/thread/PrintFirstSecondThird.java
@@ -0,0 +1,88 @@
+package focus.javabase.com.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author chenjianrong-lhq
+ * @Description 为什么使用ReentrantLock 得不到期望的答案？
+ * @Date 2020-09-13 18:14
+ **/
+public class PrintFirstSecondThird {
+
+    private static final Lock lock = new ReentrantLock(true);
+
+    public PrintFirstSecondThird() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        lock.lock();
+        try {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        lock.lock();
+        try {
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        lock.lock();
+        try {
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        PrintFirstSecondThird foo = new PrintFirstSecondThird();
+        int nums[] = new int[]{1, 2, 3, 3, 2, 1, 1, 2, 3};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                new Thread(() -> {
+                    try {
+                        foo.first(() -> {
+                            System.out.print("first");
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            } else if (nums[i] == 2) {
+                new Thread(() -> {
+                    try {
+                        foo.second(() -> {
+                            System.out.print("second");
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            } else if (nums[i] == 3) {
+                new Thread(() -> {
+                    try {
+                        foo.third(() -> {
+                            System.out.print("third");
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+            Thread.sleep(1);
+        }
+        System.out.println();
+    }
+}
