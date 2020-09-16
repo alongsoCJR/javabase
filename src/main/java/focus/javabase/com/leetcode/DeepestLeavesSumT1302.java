b933248e17f7992726bdc724e7eb4f8fbbc34733
alongsoCJR <alongsocjr@163.com>
2020-09-16 11:11:27 +0800

    层数最深叶子结点的和 参考

--- src/main/java/focus/javabase/com/leetcode/DeepestLeavesSumT1302.java
+++ src/main/java/focus/javabase/com/leetcode/DeepestLeavesSumT1302.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 
 public class DeepestLeavesSumT1302 {
 
+    private static int max = -1;
+    private static int result = 0;
+
     public static int deepestLeavesSum(TreeNode root) {
         Map<TreeNode, Integer> deepMap = new HashMap<>();
         Stack<TreeNode> stack = new Stack<>();
@@ -56,5 +59,90 @@ public class DeepestLeavesSumT1302 {
 
         deepestLeavesSum = listOfMax.stream().mapToInt(x -> (x.val)).sum();
         return deepestLeavesSum;
+    }
+
+    // DFS 深度遍历
+    // 对于node  dep(node)<max  continue
+    //          dep(node)=max sum+=node.val
+    //          dep(node)>max sum=node.val
+    public static int deepestLeavesSum0(TreeNode root) {
+        int max = Integer.MIN_VALUE;
+        int result = 0;
+        Map<TreeNode, Integer> deepMap = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        root.dep = 0;
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                node.right.dep = node.dep + 1;
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                node.left.dep = node.dep + 1;
+                stack.push(node.left);
+            }
+
+            if (node.dep < max) {
+                continue;
+            } else if (node.dep > max) {
+                max = node.dep;
+                result = node.val;
+            } else {
+                result += node.val;
+            }
+        }
+        return result;
+    }
+
+    // 参考
+    public static int deepestLeavesSum1(TreeNode root) {
+        dfs(root, 0);
+        return result;
+    }
+
+
+    // 对于node  dep(node)<max  continue
+    //          dep(node)=max sum+=node.val
+    //          dep(node)>max sum=node.val
+    private static void dfs(TreeNode root, int dep) {
+        if (root != null) {
+            if (dep > max) {
+                max = dep;
+                result = root.val;
+            } else if (dep == max) {
+                result += root.val;
+            }
+            dfs(root.left, dep + 1);
+            dfs(root.right, dep + 1);
+        }
+    }
+
+    // 层次遍历更简单
+    public static int deepestLeavesSum2(TreeNode root) {
+        return bfs(root, 0);
+    }
+
+    private static int bfs(TreeNode root, int dep) {
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+        int sum = 0;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = deque.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+        }
+        return sum;
     }
 }
