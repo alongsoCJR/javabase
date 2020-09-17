0ff83115635f544bc3df25a9bc93880c508f8944
alongsoCJR <alongsocjr@163.com>
2020-09-17 14:47:49 +0800

    顺序执行 多线程代码

---
+++ src/main/java/focus/javabase/com/leetcode/FooBarT1115.java
@@ -0,0 +1,89 @@
+package focus.javabase.com.leetcode;
+
+import java.util.concurrent.Semaphore;
+
+public class FooBarT1115 {
+
+    private int n;
+    private Semaphore fooSemaphore = new Semaphore(1);
+    private Semaphore barSemaphore = new Semaphore(0);
+
+    public FooBarT1115(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            fooSemaphore.acquire();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            barSemaphore.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            barSemaphore.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            fooSemaphore.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        FooBar fooBar = new FooBar(2);
+        new Thread(() -> {
+            try {
+                fooBar.foo(() -> {
+                    System.out.print("foo");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                fooBar.bar(() -> {
+                    System.out.print("bar");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+}
+
+class FooBar {
+
+    private int n;
+    private volatile boolean foo = true;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            while (!foo) {
+                Thread.yield();  // sleep ?
+            }
+            printFoo.run();
+            foo = false;
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            while (foo) {
+                Thread.yield();
+            }
+            printBar.run();
+            foo = true;
+        }
+    }
+}
