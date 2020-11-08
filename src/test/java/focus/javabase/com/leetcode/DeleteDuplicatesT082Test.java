fb73c53aa997fb272f1f712a0a14ff396af33e67
chenjianrong <chenjianrong@fenbi.com>
2020-11-08 21:50:10 +0800

    排序链表去重 II

--- src/test/java/focus/javabase/com/leetcode/DeleteDuplicatesT082Test.java
+++ src/test/java/focus/javabase/com/leetcode/DeleteDuplicatesT082Test.java
@@ -10,18 +10,57 @@ public class DeleteDuplicatesT082Test extends AbstractTest {
     @Before
     public void setUp() throws Exception {
         inputList = new ListNode(1);
-        inputList.next = new ListNode(2);
+        inputList.next = new ListNode(1);
         inputList.next.next = new ListNode(2);
-        inputList.next.next.next = new ListNode(3);
-        inputList.next.next.next.next = new ListNode(3);
-        inputList.next.next.next.next.next = new ListNode(4);
-        inputList.next.next.next.next.next.next = new ListNode(4);
-        inputList.next.next.next.next.next.next.next = new ListNode(5);
+        inputList.next.next.next = new ListNode(2);
+//        inputList.next.next.next.next = new ListNode(3);
+//        inputList.next.next.next.next.next = new ListNode(4);
+//        inputList.next.next.next.next.next.next = new ListNode(4);
+//        inputList.next.next.next.next.next.next.next = new ListNode(5);
 
     }
 
+//    @Before
+    public void setUp0() throws Exception {
+        inputList = new ListNode(1);
+        inputList.next = new ListNode(1);
+        inputList.next.next = new ListNode(1);
+        inputList.next.next.next = new ListNode(2);
+        inputList.next.next.next.next = new ListNode(3);
+    }
+
+    // info
+    //					Compile Error:
+    //					java.lang.NullPointerException
+    //					at line 33, Solution.deleteDuplicates
+    //					at line 57, __DriverSolution__.__helper__
+    //					at line 85, __Driver__.main
+    //					Your input:[1,1,1,2,3]
+    //					stdout:
+
+
+    // info
+    //					Wrong Answer:
+    //					input:[1,2,2]
+    //					Output:[1,2,2]
+    //					Expected:[1]
+    //					stdout:
+
+
+    // info
+    //					Wrong Answer:
+    //					input:[1,1,2,2]
+    //					Output:[2]
+    //					Expected:[]
+    //					stdout:
     @Test
     public void testDeleteDuplicates() {
-        outputList = DeleteDuplicatesT082.deleteDuplicates(inputList);
+        outputList = DeleteDuplicatesT082.deleteDuplicates0(inputList);
+    }
+
+
+    @Test
+    public void testDeleteDuplicates1() {
+        outputList = DeleteDuplicatesT082.deleteDuplicates1(inputList);
     }
 }
