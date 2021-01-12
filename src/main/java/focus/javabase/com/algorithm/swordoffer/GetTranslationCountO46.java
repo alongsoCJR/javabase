0efb432ccfdac93f18606d39ad9638872a77f889
chenjianrong <chenjianrong@fenbi.com>
2021-01-12 10:41:51 +0800

    把数字翻译成字符串 测试案例

---
+++ src/main/java/focus/javabase/com/algorithm/swordoffer/GetTranslationCountO46.java
@@ -0,0 +1,13 @@
+package focus.javabase.com.algorithm.swordoffer;
+
+/**
+ * @Author Jianrong.Chen
+ * @Description 把数字翻译成字符串
+ * @Date 2021/1/12 10:34 AM
+ */
+public class GetTranslationCountO46 {
+
+    public static int getTranslationCount(int number) {
+        return -1;
+    }
+}
---
+++ src/test/java/focus/javabase/com/algorithm/swordoffer/GetTranslationCountO46Test.java
@@ -0,0 +1,23 @@
+package focus.javabase.com.algorithm.swordoffer;
+
+import focus.javabase.com.baseTest.AbstractTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+// 输入一个数字，翻译成字母（0-a，1-b,2-c,,,25-z），求一共有多少种翻译
+// 比如：12258 bccfi、bwfi、bczi、mcfi、mzi 共5种
+public class GetTranslationCountO46Test extends AbstractTest {
+
+    @Before
+    public void setUp() throws Exception {
+        inputNum = 122258;
+        exceptedNum = 5;
+    }
+
+    @Test
+    public void testGetTranslationCount() {
+        Assert.assertEquals(exceptedNum, GetTranslationCountO46.getTranslationCount(inputNum));
+    }
+}
